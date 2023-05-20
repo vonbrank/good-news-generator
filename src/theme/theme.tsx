3988829e56a764e0a942faa202c223ac57865fd2
vonbrank <vonbrank@outlook.com>
2023-05-20 13:51:55 +0800

    chore: 添加 theme 配置

---
+++ src/theme/theme.tsx
@@ -0,0 +1,18 @@
+import { createTheme } from "@mui/material/styles";
+
+export const lightTheme = createTheme({
+  typography: {
+    htmlFontSize: 10,
+  },
+  palette: {
+    mode: "light",
+  },
+});
+export const darkTheme = createTheme({
+  typography: {
+    htmlFontSize: 10,
+  },
+  palette: {
+    mode: "dark",
+  },
+});
